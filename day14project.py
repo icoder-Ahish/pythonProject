5a95e9c3317bd47c859a18458801f0aeaa47ecf7
Ashish <ashishsahu81144@gmail.com>
2023-04-20 08:44:11 +0530

    code updated commit

--- day14project.py
+++ day14project.py
@@ -1,19 +1,21 @@
 import streamlit as st
 import pandas as pd
+import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
-# st.header('`streamlit_pandas_profiling`')
-#
-# df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-#
-# pr = df.profile_report()
-# st_profile_report(pr)
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
+
 ############## My Code ##################
 
 # Load a sample dataset
-st.header('`My Streamlit data now showing into dashboard streamlit_pandas_profiling`')
-
-df2 = pd.read_csv("data.csv")
-
-pr2 = df2.profile_report()
-st_profile_report(pr2)
+# st.header('`My Streamlit data now showing into dashboard streamlit_pandas_profiling`')
+#
+# df2 = pd.read_csv("data.csv")
+#
+# pr2 = df2.profile_report()
+# st_profile_report(pr2)
